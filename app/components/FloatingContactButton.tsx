59d2b496d0c7243c059feb9e016cbdfe03cf0ef4
Bikari731 <bukariservices@gmail.com>
2025-08-11 22:54:40 +0300

    Update FloatingContactButton.tsx

--- app/components/FloatingContactButton.tsx
+++ app/components/FloatingContactButton.tsx
@@ -5,7 +5,8 @@ export default function FloatingContactButton() {
   return (
     <Link
       href="https://wa.me/+971501113815"
-      className="fixed bottom-6 right-6 z-[9999] hover:bg-white text-white p-1 rounded-full hover:shadow-2xl transition duration-300 border-2 border-white"
+      target='_blank'
+      className="fixed bottom-52 right-6 z-[9999] hover:bg-white text-white p-1 rounded-full hover:shadow-2xl transition duration-300 border-2 border-white"
       aria-label="Call Us"
     >
       <div className='bg-gn rounded-full p-3'>
